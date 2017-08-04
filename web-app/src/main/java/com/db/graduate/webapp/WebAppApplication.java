24ee383517c3e0eb4ee04d64512c0ea66ce9a091
Egor <yeplastun@gmail.com>
2017-08-04 16:55:22 +0100

    added spring boot

---
+++ web-app/src/main/java/com/db/graduate/webapp/WebAppApplication.java
@@ -0,0 +1,12 @@
+package com.db.graduate.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+}
