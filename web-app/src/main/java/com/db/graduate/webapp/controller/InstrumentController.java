dab12d5564947e1ca0ac5d14193f4cc42b96fbca
Egor <yeplastun@gmail.com>
2017-08-07 13:35:22 +0100

    added deal/all http request

--- web-app/src/main/java/com/db/graduate/webapp/controller/InstrumentController.java
+++ web-app/src/main/java/com/db/graduate/webapp/controller/InstrumentController.java
@@ -3,7 +3,6 @@ package com.db.graduate.webapp.controller;
 import com.db.graduate.dao.Instrument;
 import com.db.graduate.webapp.DbTierService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +15,6 @@ public class InstrumentController {
     @Autowired
     public InstrumentController(DbTierService dbService) {
         this.dbService = dbService;
-    }
-
-    @RequestMapping("/hello/{name}")
-    String hello(@PathVariable String name) {
-        return "Hello, " + name + "!";
     }
 
     @RequestMapping("/instrument/all")
